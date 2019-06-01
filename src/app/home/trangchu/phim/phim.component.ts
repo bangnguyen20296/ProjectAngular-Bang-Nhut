26b976fc5294cd6dae420d35cc9bfb3d8c44bbb2
Hai Bang <bangnguyen20296@gmail.com>
2019-06-01 11:03:04 +0700

    pagination for list films

--- src/app/home/trangchu/phim/phim.component.ts
+++ src/app/home/trangchu/phim/phim.component.ts
@@ -3,17 +3,29 @@ import { LayDanhSachPhimService } from 'src/app/service/phim/lay-danh-sach-phim.
 import { Subscription } from 'rxjs';
 import { Phim } from 'src/app/model/phim';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { animate, trigger, transition, style, sequence } from '@angular/animations';
 
 @Component({
   selector: 'app-phim',
   templateUrl: './phim.component.html',
-  styleUrls: ['./phim.component.css']
+  styleUrls: ['./phim.component.css'],
+  animations: [
+    trigger('anim', [
+      transition('void => *', [
+        style({ opacity: '0.7', transform: 'translateY(5px)' }),
+        sequence([
+          animate('1.3s ease', style({ opacity: '1', transform: 'translateY(0px)' })),
+        ])
+      ])
+    ])
+  ]
 })
 export class PhimComponent implements OnInit, OnDestroy {
   subService: Subscription;
   dsPhim: Phim[] = [];
   urlTrailer: SafeResourceUrl;
   public trailerPhim: any;
+  p = 1;
   constructor(private layDSPhim: LayDanhSachPhimService, private sanz: DomSanitizer) {
 
   }
